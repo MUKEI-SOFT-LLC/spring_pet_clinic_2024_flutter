eff9b64f2b59878fa86dda3c019cea2173847060
om-mks <murayama@mukei-soft.co.jp>
2021-07-12 12:04:52 +0900

    changed inheritance to delegation.

--- lib/dio/pet_clinic_rest_client.dart
+++ lib/dio/pet_clinic_rest_client.dart
@@ -1,56 +1,53 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'package:async/async.dart';
 
 import 'package:dio/dio.dart';
-import 'package:dio/native_imp.dart';
 import 'package:spring_pet_clinic_2021_flutter/entity/owner.dart';
 import 'package:spring_pet_clinic_2021_flutter/entity/pet.dart';
 import 'package:spring_pet_clinic_2021_flutter/entity/pet_type.dart';
 import 'package:spring_pet_clinic_2021_flutter/entity/specialty.dart';
 import 'package:spring_pet_clinic_2021_flutter/entity/veterinarian.dart';
 
-class PetClinicRestClient extends DioForNative {
-  PetClinicRestClient()
-      : super(BaseOptions(
-            baseUrl:
-                'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:9966/petclinic/api'));
+class PetClinicRestClient {
+  final Dio _dio = Dio(BaseOptions(
+      baseUrl:
+      'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:9966/petclinic/api'));
 
   Stream<List<Pet>> get allPets {
-    return super.get('/pets').asStream().map((response) =>
+    return _dio.get('/pets').asStream().map((response) =>
         (response.data as List).map((e) => Pet.fromJson(e)).toList());
   }
 
   Stream<List<Owner>> get allOwners {
-    return super.get('/owners').asStream().map((response) =>
+    return _dio.get('/owners').asStream().map((response) =>
         (response.data as List).map((e) => Owner.fromJson(e)).toList());
   }
 
   Stream<List<Veterinarian>> get allVets {
-    return super.get('/vets').asStream().map((response) =>
+    return _dio.get('/vets').asStream().map((response) =>
         (response.data as List).map((e) => Veterinarian.fromJson(e)).toList());
   }
 
   Stream<Response> update(Pet pet) {
     final json = jsonEncode(pet);
-    return super.put('/pets/${pet.id}', data: json).asStream();
+    return _dio.put('/pets/${pet.id}', data: json).asStream();
   }
 
   Stream<Response> save(Pet pet) {
     final json = jsonEncode(pet);
-    return super.post('/pets', data: json).asStream();
+    return _dio.post('/pets', data: json).asStream();
   }
 
   Future<List<Specialty>> get specialties async {
-    final response = await super.get('/specialties');
+    final response = await _dio.get('/specialties');
     return (response.data as List)
         .map((json) => Specialty.fromJson(json))
         .toList();
   }
 
   Future<List<PetType>> get petTypes async {
-    final response = await super.get('/pettypes');
+    final response = await _dio.get('/pettypes');
     return (response.data as List)
         .map((json) => PetType.fromJson(json))
         .toList();
